2c4369e08bc09920b6673e071f784af45c593b1e
kuchnaya-hub <75515289+kuchnaya-hub@users.noreply.github.com>
2021-03-28 15:20:27 +0530

    Add Support For Hindi Characters
    Support Hindi Characters

--- lottie/src/main/java/com/airbnb/lottie/model/layer/TextLayer.java
+++ lottie/src/main/java/com/airbnb/lottie/model/layer/TextLayer.java
@@ -450,6 +450,7 @@ public class TextLayer extends BaseLayer {
         Character.getType(codePoint) == Character.MODIFIER_SYMBOL ||
         Character.getType(codePoint) == Character.NON_SPACING_MARK ||
         Character.getType(codePoint) == Character.OTHER_SYMBOL ||
+        Character.getType(codePoint) == Character.DIRECTIONALITY_NONSPACING_MARK ||
         Character.getType(codePoint) == Character.SURROGATE;
   }
 
